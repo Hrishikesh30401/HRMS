6fd72ee25780c37297180131cdee06afd425d2d4
ganesh <ganeshsatpute1999>
2025-01-30 11:05:09 +0530

    text changes

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('HRMS'),
         ),
-        body: Text('HRMS app body'),
+        body: Text('HRMS apps body'),
       ),
     );
   }
